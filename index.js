d24aa1a8d9111468741444100fb6d6ad8b670fbe
Michalzard <miso.platko@gmail.com>
2022-05-29 02:31:48 +0200

    added db,dotenv

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const {Client,Intents} = require("discord.js");
-const client = new Client({intents:[Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES]});
-require("dotenv").config();
-//init
-client.on('ready',()=>{
-    console.log(`Logged in as ${client.user.tag}`);
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
+require("dotenv").config();
 
+const mongoose = require("mongoose");
+
+//init
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  mongoose
+    .connect(process.env.DB_URI)
+    .then(() => {
+      console.log("Logged in a database");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
 
 client.login(`${process.env.BOT_TOKEN}`);
 
-client.on('messageCreate',(msg)=>{
-    if(!msg.author.bot)
-    msg.reply('debugging');
-})
+const { MessageHandler } = require("./utils/messageHandler");
+
+client.on("messageCreate", (msg) => {
+  MessageHandler.handle(msg);
+});
