8847c0dcbf8bb202078fb8498ca1b64cb6d7c703
Michalzard <miso.platko@gmail.com>
2022-06-22 23:22:24 +0200

    icon to hold discord emoji data,quantity for stacking

--- schemas/item.js
+++ schemas/item.js
@@ -6,25 +6,35 @@ const ItemSchema = new mongoose.Schema({
     type: Number,
     default: 1,
   },
+  icon: {
+    name: {
+      type: String,
+      default: "icon_name",
+    },
+    id:{
+      type:String,
+      default:"123456789",
+    }
+  },
   name: {
     type: String,
     default: "Item Name",
   },
-  quantity:{
-    type:Number,
-    default:1,
+  quantity: {
+    type: Number,
+    default: 1,
   },
-  quantityMax:{
-    type:Number,
-    default:5,
+  quantityMax: {
+    type: Number,
+    default: 5,
   },
-  rarity:{
-    type:String,
-    enum:["common,rare,epic,legendary"],
-    default:"common"
+  rarity: {
+    type: String,
+    enum: ["common","rare","epic","legendary"],
+    default: "common",
   },
 });
 
-ItemSchema.plugin(AutoIncrement,{id:"item_seq",inc_field:"item_id"});
+ItemSchema.plugin(AutoIncrement, { id: "item_seq", inc_field: "item_id" });
 
 module.exports = mongoose.model("Item", ItemSchema);
