d68a2820864d0caa7b718da0854beeb7f939a31d
Michalzard <miso.platko@gmail.com>
2022-06-22 23:23:48 +0200

    getEmojiList returns array with every single cached emoji object

--- utils/helpers/emojiHandler.js
+++ utils/helpers/emojiHandler.js
@@ -1,9 +1,8 @@
 //Should be used as Array of GuildEmojis
 module.exports.Emoji = class Emoji {
-  static getEmojiByName(message, name) {
+  static getEmojiList(message) {
     return message.guild.emojis.cache.map((emoji) => {
-      if (name == null || name == "") return { id: emoji.id, name: emoji.name }; // return all emojis
-      if (emoji.name === name) return { id: emoji.id, name }; //if name is specified return just that item
+      return { name:emoji.name , id: emoji.id }; //if name is specified return just that item
     }).filter(emoji=>{return emoji});//filter out undefined or missing emojis
    
   }
